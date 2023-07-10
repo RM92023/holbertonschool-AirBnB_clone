b2aa82dcf71aaa60de2ee7e5aa414cf31e987457
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-07-10 16:31:11 -0500

    Update file change variables updated and created

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
     '''Initializate the function init'''
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.update_at = self.create_at
+        self.created_at = datetime.now()
+        self.updated_at = self.create_at
 
     '''Defining the function str'''
     def __str__(self):
@@ -23,13 +23,13 @@ class BaseModel:
     '''updates the public instance attribute
     updated_at with the current datetime'''
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     '''returns a dictionary containing all keys/values
     of __dict__ of the instance'''
     def to_dict(self):
         obj = self.__dict__.copy()
         obj['__class__'] = __class__.__name__
-        obj['create_at'] = self.create_at.isoformat()
-        obj['update_at'] = self.update_at.isoformat()
+        obj['created_at'] = self.created_at.isoformat()
+        obj['updated_at'] = self.updated_at.isoformat()
         return obj
