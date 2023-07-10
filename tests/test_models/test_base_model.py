5ca4750d583ed1ea9d38943591d9129896451e39
RM92023 <robinjara20@gmail.com>
2023-07-10 17:08:38 -0500

    Update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,3 +11,7 @@ class TestBaseModel(unittest.TestCase):
         prev_updated_at = self.base_model.updated_at
         self.base_model.save()
         self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
