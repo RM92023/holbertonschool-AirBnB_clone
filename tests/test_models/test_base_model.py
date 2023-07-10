851847f113fb76b89868ace9cc19c6388b003aaf
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-07-10 17:14:55 -0500

    Update file test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,6 +12,14 @@ class TestBaseModel(unittest.TestCase):
         self.base_model.save()
         self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
 
+    def test_to_dict(self):
+        expected_keys = ['id', 'created_at', 'updated_at', '__class__']
+        obj = self.base_model.to_dict()
+        self.assertEqual(sorted(obj.keys()), sorted(expected_keys))
+        self.assertEqual(obj['__class__'], 'BaseModel')
+        self.assertIsInstance(obj['created_at'], str)
+        self.assertIsInstance(obj['updated_at'], str)
+
 
 if __name__ == "__main__":
     unittest.main()
