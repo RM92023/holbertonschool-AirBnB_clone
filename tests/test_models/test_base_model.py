669ac7f130cd4cd7add12b4d0f614f94dda1930b
RM92023 <robinjara20@gmail.com>
2023-07-11 08:29:05 -0500

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,6 +11,12 @@ class TestBaseModel(unittest.TestCase):
         prev_updated_at = self.base_model.updated_at
         self.base_model.save()
         self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
+    
+    def test_id(self):
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_created_at(self):
+        self.assertIsInstance(self.base_model.created_at, datetime)
 
 
 if __name__ == "__main__":
