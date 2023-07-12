c58fcdbeded27aeb834ab7906c7a8d61af79a267
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-07-12 14:00:10 -0500

    Update file call test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,62 +1,70 @@
 import unittest
+import json
 from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestBaseModel(unittest.TestCase):
-    """
-    Unit tests for the BaseModel class.
-    """
-
+class TestFileStorage(unittest.TestCase):
     def setUp(self):
-        """
-        Set up a BaseModel instance for testing.
-        """
-        self.model = BaseModel()
+        self.storage = FileStorage()
+        self.base_model = BaseModel()
 
-    def test_save_updates_updated_at(self):
-        """
-        Test that the save() method updates the updated_at attribute.
-        """
-        previous_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(previous_updated_at, self.model.updated_at)
+    def test_file_path(self):
+        self.assertEqual(self.storage._FileStorage__file_path, "file.json")
 
-    def test_to_dict_returns_dict(self):
-        """
-        Test that the to_dict() method returns a dictionary.
-        """
-        obj_dict = self.model.to_dict()
-        self.assertIsInstance(obj_dict, dict)
+    def test_objects(self):
+        self.assertIsInstance(self.storage._FileStorage__objects, dict)
 
-    def test_to_dict_contains_expected_keys(self):
-        """
-        Test that the to_dict() method returns a dictionary with expected keys.
-        """
-        expected_keys = ['id', 'created_at', 'updated_at', '__class__']
-        obj_dict = self.model.to_dict()
-        for key in expected_keys:
-            self.assertIn(key, obj_dict)
+    def test_all(self):
+        objects = self.storage.all()
+        self.assertEqual(objects, self.storage._FileStorage__objects)
 
-    def test_id_is_string(self):
-        """
-        Test that the id attribute is a string.
-        """
-        self.assertIsInstance(self.model.id, str)
+    def test_new(self):
+        key = "{}.{}".format(
+            self.base_model.__class__.__name__, self.base_model.id)
+        self.storage.new(self.base_model)
+        self.assertIn(key, self.storage._FileStorage__objects)
+        self.assertEqual(
+            self.storage._FileStorage__objects[key], self.base_model)
 
-    def test_created_at_is_datetime(self):
-        """
-        Test that the created_at attribute is a datetime object.
-        """
-        self.assertIsInstance(self.model.created_at, datetime)
+    def test_save(self):
+        key = "{}.{}".format(
+            self.base_model.__class__.__name__, self.base_model.id)
+        self.storage.new(self.base_model)
+        self.storage.save()
+        with open("file.json", 'r') as file:
+            data = file.read()
+            self.assertIn(key, data)
 
-    def test_str_representation(self):
-        """
-        Test the __str__() method for the BaseModel class.
-        """
-        expected_string = f"[BaseModel] ({self.model.id}) {self.model.__dict__}"
-        self.assertEqual(str(self.model), expected_string)
+    def test_reload(self):
+        key = "{}.{}".format(
+            self.base_model.__class__.__name__, self.base_model.id)
+        obj_dict = {key: self.base_model.to_dict()}
+        with open("file.json", 'w') as file:
+            file.write(json.dumps(obj_dict))
+        self.storage.reload()
+        self.assertIn(key, self.storage._FileStorage__objects)
+        self.assertEqual(
+            self.storage._FileStorage__objects[key].id, self.base_model.id)
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_init(self):
+        self.base_model.save()
+        key = "{}.{}".format(
+            self.base_model.__class__.__name__, self.base_model.id)
+        self.assertIn(key, self.storage._FileStorage__objects)
+        self.assertEqual(
+            self.storage._FileStorage__objects[key], self.base_model)
+
+    def test_save(self):
+        prev_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
 
 
 if __name__ == '__main__':
