becf3b33329e3a40508ee2db2a34bf5041aa0ac0
RM92023 <robinjara20@gmail.com>
2023-07-12 14:09:29 -0500

    Update test again

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -29,42 +29,49 @@ class TestFileStorage(unittest.TestCase):
             self.storage._FileStorage__objects[key], self.base_model)
 
     def test_save(self):
-        key = "{}.{}".format(
-            self.base_model.__class__.__name__, self.base_model.id)
-        self.storage.new(self.base_model)
+        """
+        Test that save() method saves the objects to the file.
+        """
+        my_model = BaseModel()
+        self.storage.new(my_model)
         self.storage.save()
-        with open("file.json", 'r') as file:
+        with open(self.storage._FileStorage__file_path, 'r') as file:
             data = file.read()
-            self.assertIn(key, data)
+        self.assertNotEqual(data, "")
 
     def test_reload(self):
-        key = "{}.{}".format(
-            self.base_model.__class__.__name__, self.base_model.id)
-        obj_dict = {key: self.base_model.to_dict()}
-        with open("file.json", 'w') as file:
-            file.write(json.dumps(obj_dict))
+        """
+        Test that reload() method reloads the objects from the file.
+        """
+        my_model = BaseModel()
+        self.storage.new(my_model)
+        self.storage.save()
         self.storage.reload()
-        self.assertIn(key, self.storage._FileStorage__objects)
-        self.assertEqual(
-            self.storage._FileStorage__objects[key].id, self.base_model.id)
+        all_objs = self.storage.all()
+        key = "{}.{}".format(type(my_model).__name__, my_model.id)
+        self.assertIn(key, all_objs)
 
 
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.base_model = BaseModel()
-
     def test_init(self):
-        self.base_model.save()
-        key = "{}.{}".format(
-            self.base_model.__class__.__name__, self.base_model.id)
-        self.assertIn(key, self.storage._FileStorage__objects)
-        self.assertEqual(
-            self.storage._FileStorage__objects[key], self.base_model)
+        """
+        Test that __init__() initializes the instance correctly.
+        """
+        my_model = BaseModel(name="Test", value=10)
+        self.assertEqual(my_model.name, "Test")
+        self.assertEqual(my_model.value, 10)
+        self.assertTrue(hasattr(my_model, "id"))
+        self.assertTrue(hasattr(my_model, "created_at"))
+        self.assertTrue(hasattr(my_model, "updated_at"))
 
     def test_save(self):
-        prev_updated_at = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
+        """
+        Test that save() method updates the updated_at attribute.
+        """
+        my_model = BaseModel()
+        previous_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(previous_updated_at, my_model.updated_at)
 
 
 if __name__ == '__main__':
