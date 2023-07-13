e346910b747244a88ea50c58d8a07881b943d4b3
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-07-13 15:02:54 -0500

    Adding file user.py and Update file file_storage and console

--- console.py
+++ console.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""
-Module Name:
-console
 
-Module Description:
-This module contains only one Class
-
-Module Classes:
-- HBNBCommand
-
-Module Attributes:
-- None
-"""
 import cmd
 from models.base_model import BaseModel
-# from models.user import User
+from models.user import User
 # from models.place import Place
 # from models.state import State
 # from models.city import City
@@ -28,32 +16,21 @@ import inspect
 class_names_str = [
     "BaseModel", "User", "Place", "State",
     "City", "Amenity", "Review"
-    ]
+]
 all_data = storage.all()
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command-line interface for the AIRBNB project."""
 
-    # intro = "Welcome to the AIRBNB console command"
     prompt = "(hbnb) "
 
     def do_quit(self, args: str) -> bool:
-        """
-        Quit command to exit the program.
-        """
         return True
 
     def do_EOF(self, args: str) -> bool:
-        """
-        Handle the end-of-file event (Ctrl+D).
-        """
         return True
 
     def do_create(self, args: str) -> None:
-        """
-        Create a new instance of a given class.
-        """
         arg_list = args.split()
         if not arg_list:
             print("** class name missing **")
@@ -63,16 +40,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
-        # Process
         new_instance = eval(class_name)()
 
         new_instance.save()
         print(new_instance.id)
 
     def do_show(self, args: str) -> None:
-        """
-        Show the string representation of an instance.
-        """
         arg_list = args.split()
         if not arg_list:
             print("** class name missing **")
@@ -89,7 +62,6 @@ class HBNBCommand(cmd.Cmd):
 
         instance_id = arg_list[1]
 
-        # Process
         model = all_data.get(f"{class_name}.{instance_id}", None)
 
         if model is None:
@@ -99,28 +71,23 @@ class HBNBCommand(cmd.Cmd):
         print(model)
 
     def do_all(self, args: Optional[str]) -> None:
-        """
-        Show the string representation of all instances of a given class.
-        """
+
         arg_list = args.split()
         if arg_list and arg_list[0] not in class_names_str:
             print("** class doesn't exist **")
             return
-        try:  # if only write all
+        try:
             class_name = arg_list[0]
         except Exception:
             pass
 
-        # Process
         objects = [str(obj) for obj in all_data.values()  # if only write all
                    if args == "" or str(obj).startswith(f"[{class_name}]")]
 
         print(objects)
 
     def do_destroy(self, args: str) -> None:
-        """
-        Delete an instance based on the class name and ID.
-        """
+
         arg_list = args.split()
         if not arg_list:
             print("** class name missing **")
@@ -137,7 +104,6 @@ class HBNBCommand(cmd.Cmd):
 
         instance_id = arg_list[1]
 
-        # Process
         try:
             all_data.pop(f"{class_name}.{instance_id}")
         except KeyError:
@@ -147,9 +113,7 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_update(self, args: str) -> None:
-        """
-        Update an instance based on the class name and ID.
-        """
+
         arg_list = args.split()
         if not arg_list:
             print("** class name missing **")
@@ -208,157 +172,139 @@ class HBNBCommand(cmd.Cmd):
         instance.save()
 
     def complete_add(self, text: str) -> str:
-        """
-        Provide auto-completion for some commands.
-        """
+
         options = [
             'quit', 'help', 'all', 'show', 'destroy', 'update', 'BaseModel',
             'User', 'Place', 'State', 'City', 'Amenity', 'Review'
-            ]
+        ]
         if text:
             return [option for option in options if option.startswith(text)]
         else:
             return options
 
     def default(self, line: str) -> None:
-        """
-        Handle unknown commands.
-        """
+
         print_string = f"Command '{line}' not found, "
         print_string += f"please type help to display the commands availables"
         print(print_string)
-    
+
+
 def emptyline(self) -> None:
-        """
-        Handle empty lines
-        """
-        pass
+
+    pass
+
 
 def do_count(self, args: str) -> None:
-        """
-        Retrieve the number of instances of a class.
-        """
-        arg_list = args.split()
-        if not arg_list:
-            print("** class name missing **")
-            return
-        if arg_list and arg_list[0] not in class_names_str:
-            print("** class doesn't exist **")
-            return
-        class_count = 0
-        for key in all_data.keys():
-            to_compare = key.split('.')[0]
-            if to_compare == arg_list[0]:
-                class_count += 1
-        print(class_count)
+
+    arg_list = args.split()
+    if not arg_list:
+        print("** class name missing **")
+        return
+    if arg_list and arg_list[0] not in class_names_str:
+        print("** class doesn't exist **")
+        return
+    class_count = 0
+    for key in all_data.keys():
+        to_compare = key.split('.')[0]
+        if to_compare == arg_list[0]:
+            class_count += 1
+    print(class_count)
+
 
 def _parse_args(self, arguments: str) -> Tuple[str, str]:
-        """
-        Parse the line enter by the user.
-        """
-        # Parse the arguments
-        try:
-            method = arguments.split('(')[0].strip('.')
-            raw_args = arguments.split('(')[1].strip(')')
 
-            is_dict = False
-            for i in raw_args:
-                if i == '{':
-                    is_dict = True
-            if is_dict:
-                line_parse = raw_args.split('{')
-                id_string = line_parse[0].replace('"', '').replace(",", "")
-                dict = "{" + line_parse[1]
-                args = f"{id_string} {dict}"
-            else:
-                ag_lt = raw_args.split(", ")
-                if len(ag_lt) == 3:
-                    if isinstance(eval(ag_lt[2]), int):
-                        args = (raw_args.replace(',', '')).replace('"', '')
-                    else:
-                        arg = (ag_lt[0] + " " + ag_lt[1]).replace('"', '')
-                        args = arg + " " + ag_lt[2]
-                else:
-                    args = (raw_args.replace(',', '')).replace('"', '')
-        except Exception as e:
-            print("Syntax Error")
-            print("Error: ", e)
-            return
+    try:
+        method = arguments.split('(')[0].strip('.')
+        raw_args = arguments.split('(')[1].strip(')')
 
-        callerframerecord = inspect.stack()[1]
-        # gets the frame of the above function
-        frame = callerframerecord[0]
-        # gets information about the framework of the above function
-        info = inspect.getframeinfo(frame)
-
-        # assign the name of the above function
-        name_function = info.function.strip("do_")
-
-        # Obtaining the internal args
-        if args != "":
-            internal_args = f"{name_function} {args}"
+        is_dict = False
+        for i in raw_args:
+            if i == '{':
+                is_dict = True
+        if is_dict:
+            line_parse = raw_args.split('{')
+            id_string = line_parse[0].replace('"', '').replace(",", "")
+            dict = "{" + line_parse[1]
+            args = f"{id_string} {dict}"
         else:
-            internal_args = f"{name_function}"
+            ag_lt = raw_args.split(", ")
+            if len(ag_lt) == 3:
+                if isinstance(eval(ag_lt[2]), int):
+                    args = (raw_args.replace(',', '')).replace('"', '')
+                else:
+                    arg = (ag_lt[0] + " " + ag_lt[1]).replace('"', '')
+                    args = arg + " " + ag_lt[2]
+            else:
+                args = (raw_args.replace(',', '')).replace('"', '')
+    except Exception as e:
+        print("Syntax Error")
+        print("Error: ", e)
+        return
 
-        return (method, internal_args)
+    callerframerecord = inspect.stack()[1]
+
+    frame = callerframerecord[0]
+
+    info = inspect.getframeinfo(frame)
+
+    name_function = info.function.strip("do_")
+
+    if args != "":
+        internal_args = f"{name_function} {args}"
+    else:
+        internal_args = f"{name_function}"
+
+    return (method, internal_args)
+
 
 def _execute(self, method: str, internal_args: str) -> None:
-        """
-        Execute the command specified
-        """
-        try:
-            eval("self.do_{}".format(method))(internal_args)
-        except Exception:
-            print("Be sure that the argument is valid")
+
+    try:
+        eval("self.do_{}".format(method))(internal_args)
+    except Exception:
+        print("Be sure that the argument is valid")
+
 
 def do_BaseModel(self, arguments: str) -> None:
-        """
-        Handle all methods of BaseModel that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_User(self, arguments: str) -> None:
-        """
-        Handle all methods of User that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_Place(self, arguments: str) -> None:
-        """
-        Handle all methods of Place that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_Amenity(self, arguments: str) -> None:
-        """
-        Handle all methods of Amenity that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_City(self, arguments: str) -> None:
-        """
-        Handle all methods of City that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_Review(self, arguments: str) -> None:
-        """
-        Handle all methods of Review that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
+
 
 def do_State(self, arguments: str) -> None:
-        """
-        Handle all methods of State that are enter in this way:
-        """
-        method, internal_args = self._parse_args(arguments)
-        self._execute(method, internal_args)
+
+    method, internal_args = self._parse_args(arguments)
+    self._execute(method, internal_args)
 
 
 if __name__ == '__main__':
